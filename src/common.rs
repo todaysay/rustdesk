13cb50a77c76b994d212fe3f79cf2d52de111f37
ait-lqm <lqm@mail.alpha-it-system.com>
2025-06-13 15:26:16 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1003,7 +1003,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk.a-it.jp".to_owned()
 }
 
 #[inline]
